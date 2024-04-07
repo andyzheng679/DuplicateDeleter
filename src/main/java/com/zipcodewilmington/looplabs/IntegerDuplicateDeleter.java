0fdbd744a5e27ee1c3311b151e5e711f527a03c3
andyzheng679 <andyzheng679@gmail.com>
2024-04-07 17:22:25 -0400

    removeDup

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,47 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+        int newArraySizeCounter = 0;
+        for(int i = 0; i < array.length; i++){
+            int dupCounter = 0;
+            for(int j = 0; j < array.length; j++){
+                if(array[j] == array[i]){
+                    dupCounter++;
+                }
+            }if(dupCounter >= maxNumberOfDuplications){
+                newArraySizeCounter++;
+            }
+        }
+
+        Integer[] newArray = new Integer[array.length - newArraySizeCounter];
+        int index = 0;
+
+        for(int i = 0; i < array.length; i++){
+            int dupCounter = 0;
+            for(int j = 0; j < array.length; j++){
+                if(array[j] == array[i]){
+                    dupCounter++;
+                }
+            }if(dupCounter < maxNumberOfDuplications){
+                newArray[index] = array[i];
+                index++;
+            }
+        }
+
+
+        return newArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        return new Integer[0];
+    }
 }
